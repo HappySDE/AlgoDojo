5061dd71f0cf9316c51011204a507cfebb5ddd15
HappySDE <HappySDE@gmail.com>
2017-08-16 20:34:41 +0300

    + http://practice.geeksforgeeks.org/problems/merge-2-sorted-linked-list-in-reverse-order/1

--- Dojo/GfG/SLL/g020.cpp
+++ Dojo/GfG/SLL/g020.cpp
@@ -1,9 +1,27 @@
-// #yyy_FILL_ME: 0_Diff_<Easy,Medium,Hard,Basic,School
-// #yyy_FILL_ME: 0_Comp_
-// #yyy_FILL_ME: hash-tags
+// #0_Diff_Easy
+// #0_Comp_Microsoft
+// #SLL
 
-/* #yyy_FILL_ME: link to task description
-#yyy_FILL_ME: full description		*/
+/* http://practice.geeksforgeeks.org/problems/merge-2-sorted-linked-list-in-reverse-order/1
+Given two linked lists sorted in increasing order. Merge them such a way that the result list is in decreasing order. Both the Linked list can be of different sizes.
+
+Constraints:
+1 <=T<= 50
+1 <= size of linked lists <= 1000
+
+Example:
+Input:
+2
+4 3
+5 10 15 40
+2 3 20
+2 2
+1 1
+2 4
+
+Output:
+40 20 15 10 5 3 2
+4 2 1 1  */
 
 #include "Headers.h"
 #include "ADT\SLL.h"
@@ -11,9 +29,72 @@ using namespace SLL;
 
 namespace {
 
-void solve()
+Node* reverse(Node* pHead)
 {
+	if (!pHead)
+		return nullptr;
 
+	Node* pPrev = pHead;
+	Node* pCurr = pHead->next;
+
+	pPrev->next = nullptr;
+	while (pCurr)
+	{
+		Node* pNext = pCurr->next;
+		pCurr->next = pPrev;
+		pPrev = pCurr;
+		pCurr = pNext;
+	}
+
+	return pPrev;
+}
+
+Node* mergeResult(Node* first, Node* second)
+{
+	first  = reverse(first);
+	second = reverse(second);
+
+	if (!first)
+	{
+		return second;
+	}
+	if (!second)
+	{
+		return first;
+	}
+
+
+	Node* pHead = nullptr;
+	if (first->data > second->data)
+	{
+		pHead = first;
+		first = first->next;
+	}
+	else
+	{
+		pHead = second;
+		second = second->next;
+	}
+
+	Node* pTail{ pHead };
+	while (first && second)
+	{
+		if (first->data > second->data)
+		{
+			pTail->next = first;
+			first = first->next;
+		}
+		else
+		{
+			pTail->next = second;
+			second = second->next;
+		}
+
+		pTail = pTail->next;
+	}
+
+	pTail->next = first ? first : second;
+	return pHead;
 }
 
 } // unnamed namespace
@@ -29,6 +110,10 @@ void g020(unsigned run)
 	cin >> T;
 	for (unsigned t = 0; t < T; ++t)
 	{
-		solve();
+		auto first = buildSll(cin);
+		auto second = buildSll(cin);
+		auto merged = mergeResult(first, second);
+		printSll(cout, merged);
+		destroySll(merged);		
 	}
 }
